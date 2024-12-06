4edf7154568a21b7b1741b75bf912f2470538626
Maze_Runner-Git <mynameisahmad4714@gmail.com>
2024-12-06 23:40:25 +0500

    Fourteenth Commit

---
+++ fa-main.cpp
@@ -0,0 +1,30 @@
+#include <iostream> 
+#include "fa-h.cpp"
+using namespace std;
+
+int main()
+  {
+	     int questions; // Number of questions on the exam 
+	     int missed;    // Number of questions missed by the student 
+	
+		     // Get the number of questions on the final exam. 
+		     cout << "How many questions are on the final exam? ";
+	    cin >> questions;
+	
+		     // Get the number of questions the student missed. 
+		     cout << "How many questions did the student miss? ";
+	     cin >> missed;
+	
+		     // Define a FinalExam object and initialize it with 
+		     // the values entered. 
+		     FinalExam test(questions, missed);
+	
+		     // Display the test results. 
+		    
+	     cout << "\nEach question counts " << test.getPointsEach()
+		 << " points.\n";
+	     cout << "The exam score is " << test.getScore() << endl;
+	     cout << "The exam grade is " << test.getLetterGrade() << endl;
+	
+		     return 0;
+	  }
