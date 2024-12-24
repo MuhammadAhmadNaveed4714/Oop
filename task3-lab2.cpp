398b280b76229015bb79b0a86eaba88c49a88093
Maze_Runner-Git <mynameisahmad4714@gmail.com>
2024-12-24 23:02:31 +0500

    29th commit

---
+++ task3-lab2.cpp
@@ -0,0 +1,37 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//struct movieData
+//{
+//	string title;
+//	string director;
+//	int yearReleased;
+//	int runningTime;
+//};
+//void displayMovieInfo(movieData movie)
+//{
+//	cout << "\nMovie title : " << movie.title;
+//	cout << "\nDirector: " << movie.director ;
+//	cout << "\nYear Released: " << movie.yearReleased ;
+//	cout << "\nRunning Time: " << movie.runningTime << " minutes" ;
+//	cout << "\n";
+//}
+//int main()
+//{
+//	movieData movie1;
+//	movieData movie2;
+//	movie1.title = "The Shawshank Redemption";
+//	movie1.director = "Frank Darabont";
+//	movie1.yearReleased = 1994;
+//	movie1.runningTime = 142;
+//	movie2.title = "The Godfather";
+//	movie2.director = "Francis Ford Coppola";
+//	movie2.yearReleased = 1972;
+//	movie2.runningTime = 177;
+//	cout << "Movie 1:" << "\n";
+//	displayMovieInfo(movie1);
+//	cout << "\nMovie 2:" << "\n";
+//	displayMovieInfo(movie2);
+//
+//	return 0;
+//}
