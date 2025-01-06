74e3ab18f8cdb29f7190f009d028ddec12848989
Maze_Runner-Git <mynameisahmad4714@gmail.com>
2025-01-06 20:07:27 +0500

    41st commit

---
+++ FileName5.cpp
@@ -0,0 +1,119 @@
+#include<iostream>
+using namespace std;
+class b1
+{
+private:
+	int bb1;
+public:
+	b1()
+	{
+		cout << "\nb1 default constructor called.\n ";
+		bb1 = -10;
+	}
+	b1(int num)
+	{
+		cout << "\nb1 overloaded constructor called.\n ";
+		bb1 = num;
+	}
+	virtual void display()
+	{
+		cout << "\nImplementing display function of b1 class.\n";
+		cout << "bb1 : " << bb1;
+	}
+	~b1()
+	{
+		cout << "\nb1 destructor called.\n";
+	}
+};
+class b2
+{
+private:
+	int bb2;
+public:
+	b2()
+	{
+		cout << "\nb2 default constructor called.\n ";
+		bb2 = -20;
+	}
+	b2(int num)
+	{
+		cout << "\nb2 overloaded constructor called.\n ";
+		bb2 = num;
+	}
+	virtual void display()
+	{
+		cout << "\nImplementing display function of b2 class.\n";
+		cout << "bb2 : " << bb2;
+	}
+	~b2()
+	{
+		cout << "\nb2 destructor called.\n";
+	}
+};
+class d :public b1, public b2
+{
+private:
+	int d1;
+public:
+	d() :b1(), b2()
+	{
+		cout << "\nd default constructor called.\n ";
+		d1 = -10;
+	}
+	d(int num, int num1, int num2) :b1(num1), b2(num2)
+	{
+		cout << "\nd overloaded constructor called.\n ";
+		d1 = num;
+	}
+	virtual void display()
+	{
+		cout << "\nImplementing display function of d class.\n";
+		b1::display();
+		b2::display();
+		cout << "\nd1 : " << d1;
+	}
+	~d()
+	{
+		cout << "\nd destructor called.\n";
+	}
+};
+int main()
+{
+	int num = 0;
+	cout << "\nEnter 1 for b1 & 2 for b2 & 3 for d : ";
+	cin >> num;
+	b1* f = nullptr;
+	b2* g = nullptr;
+	d* h = nullptr;
+	if (num == 1)
+	{
+		f = new d;
+		f->display();
+	}
+	else if (num == 2)
+	{
+		g = new d;
+		g->display();
+	}
+	else if (num == 3)
+	{
+		h = new d;
+		h->display();
+	}
+	if (num == 1)
+	{
+		delete f;
+		f = nullptr;
+	}
+	else if (num == 2)
+	{
+		delete g;
+		g = nullptr;
+	}
+	else if (num == 3)
+	{
+		delete h;
+		h = nullptr;
+	}
+	return 0;
+}
