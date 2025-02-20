8bf1b135fd840b0f44786d3d8dae1935e082a80e
Maze_Runner-Git <mynameisahmad4714@gmail.com>
2025-02-20 22:11:23 +0500

    83rd commit

---
+++ code36.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+//Function to insert a string
+int insertString(char* destination, int index, const char* src)
+{
+	int srcLength = 0;
+	while (src[srcLength] != '\0')
+	{
+		srcLength++;
+	}
+	int destinationLength = 0;
+	while (destination[destinationLength] != '\0')
+	{
+		destinationLength++;
+	}
+	//Check if index is within bounds
+	if (index<0 || index>destinationLength)
+	{
+		return -1;
+	}
+	for (int i = destinationLength; i >= index; i--)
+	{
+		destination[i + srcLength] = destination[i];
+	}
+	//Insert the new string
+	for (int i = 0; i < srcLength; i++)
+	{
+		destination[index + i] = src[i];
+	}
+	destination[destinationLength + srcLength] = '\0';
+	return destinationLength + srcLength;
+}
+int main()
+{
+	char str[20] = "HelloWorld";
+	const char* src = ", ";
+	cout << "\n\nOriginal string : " << str;
+	int newLength = insertString(str, 5, src);
+	cout << "\n\nModified string : " << str;
+	cout << "\n\nNew length : " << newLength << "\n";
+	return 0;
+}
