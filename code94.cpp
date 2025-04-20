ca33f94310f02fa74313b6383b0e70568123cb97
Maze_Runner-Git <mynameisahmad4714@gmail.com>
2025-04-20 23:43:15 +0500

    134th commit

---
+++ code94.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+// Function to find right smaller elements
+void Right(int* arr, int size)
+{
+	int* rs = new int[size];
+	rs[size - 1] = 0;
+	for (int i = size - 2; i >= 0; i--)
+	{
+		int j = i + 1;
+		while (j < size && arr[j] >= arr[i])
+		{
+			j++;
+		}
+		if (j < size)
+		{
+			rs[i] = arr[i];
+		}
+		else
+		{
+			rs[i] = 0;
+		}
+	}
+	cout << "\n\t\t\t\t\tDisplay of right-most array of elements : ";
+	for (int i = 0; i < size; i++)
+	{
+		cout << rs[i] << " ";
+	}
+	cout << "\n";
+	delete[]rs;
+	rs = nullptr;
+}
+	int main()
+	{
+		int arr[] = { 2,7,4,8,3,2 };
+		int size = sizeof(arr) / sizeof(arr[0]);
+		Right(arr, size);
+		return 0;
+	}
